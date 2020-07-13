8e64ec3e62572bee5542fd3e2a49817c709df156
poojanaidu07 <poojashetty62362@gmail.com>
2020-07-13 15:44:41 +0530

    initial commit

---
+++ FirstApp/FirstApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int x = 5;
+				int y = 6;
+			y = ++x;
+			Console.WriteLine(x);
+			Console.WriteLine(y);
+
+
+		}
+	}
+}
